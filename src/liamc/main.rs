053a1aa31aec4321924f34ff13022b4958b5335a
Jack Delahunt <20085409@mail.wit.ie>
2022-03-22 23:40:42 +0000

    Feat: basic variables

--- src/liamc/main.rs
+++ src/liamc/main.rs
@@ -3,7 +3,7 @@ extern crate pest;
 extern crate pest_derive;
 
 use pest::{Parser, iterators::{Pairs, Pair}};
-use std::fs;
+use std::{fs, collections::HashMap};
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -29,12 +29,17 @@ fn main() {
 // }
 
 struct Compiler {
-    generated_code: String
+    generated_code: String,
+    context: Context
 }
 
 impl Compiler {
     fn new() -> Self {
-        let mut s =  Self {generated_code: String::new()};
+        let mut s =  Self {
+            generated_code: String::new(),
+            context: Context::new()
+        };
+
         s.generated_code.push_str("@main\n");
         return s;
     }
@@ -56,7 +61,16 @@ impl Compiler {
         match pair.as_rule() {
             Rule::binary_expression => return Some(self.eval_binary_expression(pair)),
             Rule::print_statement => return Some(self.eval_print_statement()),
+            Rule::let_statement => return Some(self.eval_let_statement(pair)),
             Rule::EOI => None,
+            _ => panic!("Could not eval this pair {:?}", pair)
+        }
+    }
+
+    fn eval_expression(&mut self, pair: Pair<Rule>) -> String {
+        match pair.as_rule() {
+            Rule::binary_expression => return self.eval_binary_expression(pair),
+            Rule::identifier => return self.eval_identifier(pair),
             _ => panic!("Could not eval this pair {:?}", pair)
         }
     }
@@ -83,8 +97,51 @@ impl Compiler {
         return output;
     }
 
+    fn eval_identifier(&mut self, identifier: Pair<Rule>) -> String {
+        let identifier_name = String::from( identifier.as_str());
+        let register = self.context.variables.get(&identifier_name).expect(format!("No identifier with name {}", identifier_name).as_str());
+        
+
+        return format!("load {}\n", register);
+    }
+
     fn eval_print_statement(&mut self) -> String {
         return String::from("print\n");
+    }
+
+    fn eval_let_statement(&mut self, let_statement: Pair<Rule>) -> String {
+        let mut sub_pairs = let_statement.into_inner();
+
+        let identifier = sub_pairs.next().expect("No identifier in this let statement");
+        let register = self.context.declare_variable(String::from( identifier.as_str()));
+
+        // We know this is always a binary expression for now
+        let expression = sub_pairs.next().expect("No expression in this let statement");
+
+        let evaled_expression = self.eval_expression(expression);
+        return format!("{}store {}\n", evaled_expression, register);
+    }
+}
+
+struct Context {
+    variables: HashMap<String, u64>,
+    free_variable_register: u64
+}
+
+impl Context {
+    fn new() -> Self {
+        return Self {
+            variables: HashMap::new(),
+            free_variable_register: 0
+        };
+    }
+
+    fn declare_variable(&mut self, identifier: String) -> u64 {
+        let register = self.free_variable_register;
+        self.variables.insert(identifier, register);
+        self.free_variable_register += 1;
+        return register;
+
     }
 }
 
