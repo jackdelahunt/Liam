98ee040b12056e0c7414c4db07b13d9af4b8e79b
Jack Delahunt <jdealhunt@gmail.com>
2022-07-23 00:25:16 +0100

    feat: split up runtime

---
+++ liamc/runtime/io.h
@@ -0,0 +1,8 @@
+//
+// Created by Jack Delahunt on 22/07/2022.
+//
+
+#ifndef LIAM_IO_H
+#define LIAM_IO_H
+
+#endif //LIAM_IO_H
---
+++ liamc/runtime/memory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jack Delahunt on 22/07/2022.
+//
+
+#include "memory.h"
