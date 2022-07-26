60a698725546b8f6b1cc7f7ac6cf6b2c84f20575
Jack Delahunt <jdealhunt@gmail.com>
2022-07-26 23:14:15 +0100

    feat: array implementation in stdlib

--- liamc/runtime/lib.h
+++ liamc/runtime/lib.h
@@ -16,29 +16,4 @@
 
 #include "io.h"
 
-template <typename T> struct Array {
-    u64 length;
-    u64 capacity;
-    std::vector<T> data;
-};
-
-template <typename T> Array<T> make_array() {
-    u64 start_capacity = 10;
-    return Array<T>{
-        .length   = 0,
-        .capacity = start_capacity,
-        .data     = std::vector<T>(),
-    };
-}
-
-template <typename T> void array_add(Array<T> *array, const T &t) {
-    array->data.push_back(t);
-}
-
-template <typename T> T array_at(Array<T> *array, u64 index) {
-    return array->data.at(index);
-}
-
-template <typename T> u64 array_size(Array<T> *array) {
-    return array->data.size();
-}
+#include "collections.h"
