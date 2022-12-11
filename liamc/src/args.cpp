d81d58139c6a6ec601eee2d7240c65173ed4a81a
Jack Delahunt <45426048+jackdelahunt@users.noreply.github.com>
2022-12-11 03:37:51 +0000

    feat: added zen of Liam

--- liamc/src/args.cpp
+++ liamc/src/args.cpp
@@ -2,6 +2,15 @@
 
 Arguments *args = NULL;
 
+std::string zen_text = R"(
+    * There should only be one obvious way to do things
+    * Simplicity and speed should be prioritised more then anything else
+    * Longer doesn't mean less readable and shorter doesn't mean smarter
+    * A more readable solution will help you in the long run
+    * Do what is best for the problem not what is trendy
+    * Errors are not exceptional
+    * Reduce developer pain as much as possible)";
+
 void Arguments::New(int argc, char **argv) {
     args         = new Arguments{};
     auto options = new cxxopts::Options("liamc", "Liam programming language compiler");
@@ -16,6 +25,7 @@ void Arguments::New(int argc, char **argv) {
     options->add_options()("e,emit", "Print emitted C++ to stdout", cxxopts::value<bool>()->default_value("false"));
     options->add_options()("t,time", "Print times", cxxopts::value<bool>()->default_value("false"));
     options->add_options()("h,help", "See this help screen", cxxopts::value<bool>()->default_value("false"));
+    options->add_options()("z,zen", "See the zen of Liam", cxxopts::value<bool>()->default_value("false"));
     options->add_options()("T,test", "Build binary to run tests", cxxopts::value<bool>()->default_value("false"));
 
     args->result  = options->parse(argc, argv);
@@ -24,6 +34,12 @@ void Arguments::New(int argc, char **argv) {
     if (args->result.count("help"))
     {
         std::cout << options->help() << std::endl;
+        exit(0);
+    }
+
+    if (args->result.count("zen"))
+    {
+        std::cout << zen_text << std::endl;
         exit(0);
     }
 
