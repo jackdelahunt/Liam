fc5f782f9671876a14b4fcd38251b2512d5253c0
Jack Delahunt <45426048+jackdelahunt@users.noreply.github.com>
2022-12-21 18:52:50 +0000

    refactor: stripped some of the stdlib

--- liamc/stdlib/include/lib.h
+++ liamc/stdlib/include/lib.h
@@ -1,20 +1,14 @@
 #pragma once
 #include <stdlib.h>
 
-#include <fstream>
-#include <string>
-#include <vector>
-
 #include "core.cpp"
 #include "core.h"
 
-#include "memory.cpp"
 #include "memory.h"
 
 #include "builtin_string.cpp"
 #include "builtin_string.h"
 
-#include "io.cpp"
 #include "io.h"
 
 #include "os.cpp"
--- liamc/stdlib/include/memory.h
+++ liamc/stdlib/include/memory.h
@@ -4,44 +4,19 @@
 #include <iostream>
 #include <stdlib.h>
 
-#define ALLOCATOR_CHECK()                                                                                              \
-    if (Internal::allocator == NULL)                                                                                   \
-    { panic(Internal::make_str("Allocator not set")); }
-
-namespace Internal {
-struct Allocator {
-    virtual void *alloc(u64 size)                    = 0;
-    virtual void *re_alloc(void *data, u64 new_size) = 0;
-    virtual void destroy(void *ptr)                  = 0;
-};
-
-extern Allocator *allocator;
-} // namespace Internal
-
-struct Malloc : public Internal::Allocator {
-    void *alloc(u64 size) override;
-    void *re_alloc(void *data, u64 new_size) override;
-    void destroy(void *ptr) override;
-};
-
 template <typename T> T *alloc(T t) {
-    ALLOCATOR_CHECK();
-    auto ptr = (T *)Internal::allocator->alloc(sizeof(T));
+    auto ptr = (T *)malloc(sizeof(T));
     *ptr     = t;
     return ptr;
 }
 
 template <typename T> T *re_alloc(T *data, u64 new_size) {
-    ALLOCATOR_CHECK();
-    return (T *)Internal::allocator->re_alloc(data, new_size);
+    return (T *)realloc(data, new_size);
 }
 
 template <typename T> void destroy(T *t) {
-    ALLOCATOR_CHECK();
-    Internal::allocator->destroy((void *)t);
+    free(t);
 }
-
-void set_allocator(str name, u64 size);
 
 template <typename T> u64 hashcode(T *t) {
     return u64{(uint64_t)t};
