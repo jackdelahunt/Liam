3a670147914cc8f659f757908e47a06f7d3e244b
Jack Delahunt <45426048+jackdelahunt@users.noreply.github.com>
2023-02-24 18:59:48 +0000

    refactor: removed alot of the stdlib

--- liamc/stdlib/include/os.cpp
+++ liamc/stdlib/include/os.cpp
@@ -5,12 +5,3 @@ namespace Internal {
 u32 argc    = 0;
 char **argv = NULL;
 } // namespace Internal
-
-Array<str> argv() {
-    auto array = make_array<str>();
-
-    for (u32 i = 0; i < Internal::argc; i++)
-    { __append(&array, make_str(Internal::argv[i], strlen(Internal::argv[i]))); }
-
-    return array;
-}
--- liamc/stdlib/include/os.h
+++ liamc/stdlib/include/os.h
@@ -1,11 +1,8 @@
 #pragma once
 
-#include "collections.h"
 #include "core.h"
 
 namespace Internal {
 extern u32 argc;
 extern char **argv;
 } // namespace Internal
-
-Array<str> argv();
