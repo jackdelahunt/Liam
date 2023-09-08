f8a576d4d619d132da51134372ed4b351b1422b2
Jack Delahunt <jdealhunt@gmail.com>
2023-09-08 17:40:34 +0100

    feat: basic ir generation

--- gliamc/src/main.go
+++ gliamc/src/main.go
@@ -1,31 +1,37 @@
 package main
 
+import (
+	"github.com/jackdelahunt/Liam/gliamc/src/compiler"
+	"log"
+)
+
 const (
 	Code = `
 fn main() bool {
-	return 5;
+	return 5 + 5;
 }
 `
 )
 
 func main() {
-	//err := liamCLI.StartCLI()
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//}
-	//source := []rune(Code)
-	//tokenBuffer := compiler.NewLexer(source).Lex()
-	//
-	//ast, err := compiler.NewParser(source, tokenBuffer).Parse()
-	//if err != nil {
-	//	log.Fatal(err.Error())
-	//}
-	//
-	//builder := compiler.NewIRBuilder(ast)
-	//err = builder.BuildIR()
-	//if err != nil {
-	//	log.Fatal(err.Error())
-	//}
-	//
-	//builder.PrintIR()
+	source := []rune(Code)
+	tokenBuffer := compiler.NewLexer(source).Lex()
+
+	ast, err := compiler.NewParser(source, tokenBuffer).Parse()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	typedAST, err := compiler.NewTypeChecker(ast).TypeCheck()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	builder := compiler.NewIRBuilder(typedAST)
+	err = builder.BuildIR()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	builder.PrintIR()
 }
