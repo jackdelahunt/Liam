f04af93411ac0a9452d21a77953b0d1d1ce830a9
Jack Delahunt <jdealhunt@gmail.com>
2023-12-19 23:11:10 +0000

    test: tests now work

--- liamc/tests/runner.py
+++ liamc/tests/runner.py
@@ -5,31 +5,25 @@ import subprocess
 import os
 
 source_dir = os.path.dirname(__file__) + "/liam/"
-compiler_path = os.path.dirname(__file__) + "/../build/release/liamc"
+compiler_path = os.path.dirname(__file__) + "/../build/debug/liamc"
 stdlib_path = os.path.dirname(__file__) + "/../stdlib"
 core_path = os.path.dirname(__file__) + "/../core"
 
 source_files = []
-test_dirs = [source_dir + f for f in listdir(source_dir) if isdir(join(source_dir, f))]
-for test_dir in test_dirs:
-    test_dir_name = os.path.basename(test_dir)
-    sub_files = [test_dir + "/" + f for f in listdir(test_dir) if isfile(join(test_dir, f))]
-    
-    for sub_file in sub_files:
-        file_name_with_extention = os.path.basename(sub_file)
-        if test_dir_name + ".liam" == file_name_with_extention:
-            source_files.append("/" + test_dir_name + "/" + test_dir_name + ".liam")
+
+for f in listdir(source_dir):
+    if isfile(join(source_dir, f)) and f.endswith(".liam"):
+        source_files.append(join(source_dir, f))
+
 
 failed_tests_count = 0
 tests_count = len(source_files)
 
 for i, file_path in enumerate(source_files):
-
     file_name_for_output = os.path.basename(file_path)
 
-
     lines = []
-    source = open(os.path.dirname(__file__) + "/liam" + file_path).readlines()
+    source = open(file_path).readlines()
     for line in source:
         if line.startswith("//"):
             lines.append(line.strip("\n/"))
@@ -38,9 +32,7 @@ for i, file_path in enumerate(source_files):
 
     compile_output = subprocess.run([
         compiler_path,
-        "--in", "liam" + file_path,
-        "--out", "out.cpp",
-        "--stdlib", stdlib_path
+        file_path,
     ], capture_output=True)
 
     if compile_output.stderr != b'' or compile_output.returncode != 0:
@@ -52,7 +44,7 @@ for i, file_path in enumerate(source_files):
         "clang++",
         "-I", f"{stdlib_path}/include",
         "-I", core_path,
-        "-std=c++20",
+        "-std=c++17",
         "-o", "out.exe",
         "out.cpp"
     ], capture_output=True)
